4bb0495a566fcc4c60c14b79883387a9d623b515
cstirneman <cstirneman@bellarmine.edu>
2023-09-09 13:52:57 -0400

    First commit

--- src/Application.java
+++ src/Application.java
@@ -1,31 +1,42 @@
 import java.util.Scanner;
 
+/**
+ * Class that takes user input in the form of a string and tests if the string is a palindrome
+ * users can enter 1 to continue entering strings, or 2 to quit
+ * extends the PalindromeChecker class to use the isPalindrome method
+ * @author cstirneman
+ *
+ */
 public class Application extends PalindromeChecker{
 
+	/**
+	 * 
+	 * @param args
+	 */
 	public static void main(String[] args) {
-		int quitFlag = 0;
+		int quitFlag = 0; // value that will determine if the program continues or quits.
 		
 	while (quitFlag != 2) {
 		Scanner scan = new Scanner(System.in);
-		String palindrome;
+		String palindrome; //holds users input
 		
 		System.out.println("Enter your palindrome: ");
-		palindrome = scan.nextLine();
+		palindrome = scan.nextLine(); //uses scanner to take user input and put it into palindrome string variable
 		
-		boolean isPalindrome = PalindromeChecker.isPalindrome(palindrome);
+		boolean isPalindrome = PalindromeChecker.isPalindrome(palindrome); // uses isPalindrome method to test string for a palindrome
 		
 		if (isPalindrome) {
-			System.out.println(palindrome + " is a palindrome!");
+			System.out.println(palindrome + " is a palindrome!"); //prints if string is a palindrome
 			
 		}
 		else {
-			System.out.println(palindrome + " is not a palindrome!");
+			System.out.println(palindrome + " is not a palindrome!"); //prints if string is not a palindrome
 		}
 		System.out.println("Enter 1 to enter another palindrome, or enter 2 to quit.");
 		palindrome = scan.nextLine();
 		
 		if(palindrome.equals("2")) {
-			System.out.println("Thank you! Closing program.");
+			System.out.println("Thank you! Closing program."); //prints if user enters 2. closes program
 			quitFlag =+ 2;
 		}
 	}
--- src/PalindromeChecker.java
+++ src/PalindromeChecker.java
@@ -1,26 +1,39 @@
 import java.util.Stack;
 
+/**
+ * Class that uses a stack to test if a string is a palindrome(words that read the same forward and backward
+ * @author cstirneman
+ *
+ */
 public class PalindromeChecker {
 
+	/**
+	 * Method that checks if a string is a palindrome.
+	 * @param string: The string from the user that will be used to check for a palindrome.
+	 * @return true if the string is a palindrome, false if it is not.
+	 */
 	public static boolean isPalindrome(String string) {
+		//makes the string all lowercase and removes spaces
 		string = string.replaceAll("\\s+","".toLowerCase());
 		
-		Stack<Character> stack = new Stack<>();
-		int length = string.length();
-		int half = length / 2;
+		Stack<Character> stack = new Stack<>();//creating an empty stack that will hold characters
+		int length = string.length();//length of the string in charcaters
+		int half = length / 2;// half the length of the string to use to push to stack and compare
 		
+		//iterate through the first half of the array and push the characters into the stack
 		for (int i = 0; i <half; i++) {
 			stack.push(string.charAt(i));
 		}
-		if (length %2 != 0) {
+		if (length %2 != 0) { //if the length is odd, then add 1 to the half int value
 			half++;
 		}
+		//start at the value of half and iterate through the string. Pop the characters from the stack and compare to the characters
 		for (int i = half; i <length; i++) {
 			if (stack.isEmpty() || stack.pop()!= string.charAt(i)){
 					return false;
 			}
 			
 		}
-		return stack.isEmpty();
+		return stack.isEmpty(); // if string is empty, then the string is a palindrome
 	}
 }
